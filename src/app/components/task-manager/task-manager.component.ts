4152511609d87c11bd317628d019c2fb0011fd1e
Sanu Khan <sanu@28lightbulbs.com>
2021-10-25 11:54:36 +0400

    Updated Export

--- src/app/components/task-manager/task-manager.component.ts
+++ src/app/components/task-manager/task-manager.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectorRef, Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { EditSettingsModel, GanttComponent } from '@syncfusion/ej2-angular-gantt';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { EditSettingsModel, GanttComponent, Gantt } from '@syncfusion/ej2-angular-gantt';
 import { DataService } from 'src/app/service/data.service';
-import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+// declare var jsPDF: any;
 @Component({
   selector: 'app-task-manager',
   templateUrl: './task-manager.component.html',
@@ -23,18 +25,48 @@ export class TaskManagerComponent implements OnInit {
   projectStartDate: Date = new Date();
   projectEndDate: Date = new Date();
   splitterSettings: { columnIndex: number; };
+  showGanttChart: boolean = false;
 
   constructor(private dataService: DataService) {
-
-    
   }
   ngOnInit(): void {
+    // this.getRemoteTaskData()
 
   }
 
   add(): void {
     this.getRemoteTaskData()
   };
+
+  export() : void {
+    window.scrollTo(0, 0);
+    const data = document.getElementById("test") as HTMLElement; 
+    data.style.display = "inline-flex"
+    // let data = document.getElementById("maindiv");
+    console.log(data);  
+    let doc = new jsPDF('l', 'cm', 'a4'); //Generates PDF in landscape mode
+    
+    html2canvas(data, { 
+      width: data.scrollWidth, 
+      height: data.scrollHeight
+    }).then(canvas => {
+      const contentDataURL = canvas.toDataURL('image/jpeg', 1.0)
+      console.log(contentDataURL);  
+      let pdf = new jsPDF('p', 'cm', 'a4'); //Generates PDF in landscape mode
+      // let pdf = new jspdf('p', 'cm', 'a4'); //Generates PDF in portrait mode
+      data.style.display = "block"
+      const hratio = pdf.internal.pageSize.getHeight() / canvas.height
+      const wratio =  pdf.internal.pageSize.getWidth() / canvas.width;
+      let ratio = wratio > hratio ? hratio : wratio
+
+      pdf.addImage(contentDataURL, 'PNG', 0, 0, canvas.width * ratio, canvas.height * ratio);  
+      pdf.save('Filename.pdf');   
+    }); 
+  }
+  ngAfterViewInit(): void {
+    // this.getRemoteTaskData()
+    // this.showGanttChart = true;
+  }
 
   getRemoteTaskData = () => {
     this.dataService.getRemoteTask()
